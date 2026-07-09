4806bfee863e5f96c95cacd1881ba5813a598612
ShyWeyD0s <sebastianchandesu62@gmail.com>
2026-07-09 16:18:53 -0500

    ejercicio 5

--- ejercicios/src/app/components/ejercicio6/ejercicio6.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-ejercicio6',
-  imports: [],
-  templateUrl: './ejercicio6.html',
-  styleUrl: './ejercicio6.css',
-})
-export class Ejercicio6 {}
